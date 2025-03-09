237b0503f167b1a73c7084d69104de8e2c3baef5
Neha Baghele <167019723+nehabaghele01@users.noreply.github.com>
2025-03-09 23:43:25 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout<<13/2;
+    // cout<<13.2/2;
+    //    cout<<13.2*2;
+
+    // int a=10;
+    // cout<<a++;
+    // cout<<endl<<a;
+
+    // cout<<a--;
+    // cout<<endl<<a;
+
+    // Comparison operator
+    // 1 or 0
+    // == < > <= >= !=v
+
+    // int a,b;
+    // cout<<"Enter the value of a:";
+    // cin>>a;
+    // cout<<"Enter the value of b:";
+    // cin>>b;
+
+    // if(a!=b){
+    //     cout<<"yes";
+    // }
+    // else
+    // cout<<"No";
+
+    // Logical operator &&, ||, !
+
+    //  int a,b,c;
+    //  cin>>a>>b>>c;
+
+    //  if(a>b&&a>c){
+    //     cout<<"yes";
+    // }
+    // else
+    // cout<<"No";
+
+    // char name = 'a';
+
+    // if(name=='a' || name=='i' || name=='o' ||name=='e' || name=='u')
+    // cout<<"Vowels";
+    // else
+    // cout<<"Consonant";
+
+    // cout<<!0<<endl;
+
+    // bitwise operator & | ^(x-or) ~ << >>
+
+    // int ans=2&3;
+    // int ans=~10;
+    // // int ans=6>>1;
+    // cout<<ans<<endl;
+
+    // Assignment operator  = + - * / %
+
+    int a = 10;
+    a -= 15; // a=a-15;
+    cout << a;
+}
---
+++ whileloop.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // while loop
+    //     int n;
+    //     cout<<"Enter the number :";
+    //     cin>>n;
+    //    int i=1; //initialise
+    // while(i<=n) //Break
+    // {
+    //     cout<<i<<endl;
+    //     i++; //update
+
+    // }
+
+    // Break & continue statements..
+    // break
+
+    // int i;
+    // for(i=1; i<=10; i++){
+    //     if(i==4)
+    //     break;
+
+    //     cout<<i<<endl;
+    //     // cout<<i<<" ";
+    // }
+
+    // int i;
+    // for(i=1; i<=10; i++){
+    //     if(i==4)
+    //     continue;
+
+    //     cout<<i<<endl;
+
+    // }
+
+    // switch case
+
+    // int i;
+    // cout << "Enter the number:";
+    // cin >> i;
+
+    // switch (i)
+    // {
+    // case 1:
+    //     cout << "Rohit";
+    //     break;
+
+    // case 2:
+    //     cout << "Mohit";
+    //     break;
+
+    // default:
+    //     cout << "Sohit";
+    //     break;
+    // }
+
+    // scope of variable
+    // int n =10;
+    // cout<<n;
+
+    // int n=30;
+    // cout<<n;
+
+    int n = 10;
+    int i = 30;
+
+    if (i == 30)
+    {
+        int n = 20;
+        cout << n;
+    }
+}
